4ac6f3b127f03795ed31b337db956e2499eb6167
Benoit Plaideau <benoit.plaideau@gmail.com>
2018-07-05 10:26:50 +0200

    Création entitée candidat (à finir)

--- ora/src/java/javabeans/Candidat.java
+++ ora/src/java/javabeans/Candidat.java
@@ -5,10 +5,45 @@
  */
 package javabeans;
 
+import java.io.Serializable;
+import java.time.LocalDate;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  *
  * @author Ikikay
  */
-public class Candidat {
+@Entity
+@Table(name = "candidat")
+public class Candidat implements Serializable {
+
+    private static final long serialVersionUID = -5892169641074303723L;
+    @Id
+    @Column(name = "idCandidat", nullable = false, length = 255)
+    private String idCandidat;
+
+    @Column(name = "nom", nullable = false, length = 255)
+    private String nom;
+    
+    @Column(name = "prenom", nullable = false, length = 255)
+    private String prenom;
+    
+    @Column(name = "codePostal", nullable = false, length = 255)
+    private String codePostal;
+    
+    @Column(name = "ville", nullable = false, length = 255)
+    private String ville;
+    
+    @Column(name = "rue", nullable = false, length = 255)
+    private String rue;
+    
+    @Column(name = "dteNaissance", nullable = false, length = 255)
+    private LocalDate dteNaissance;
+    
+    @Column(name = "tel", nullable = false, length = 255)
+    private String tel;
     
 }
