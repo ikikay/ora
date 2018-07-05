edb09deadb3f339761a5a36ae3252723c4c6c461
Nouvel utilisateur <Nouvel utilisateur@DESKTOP-U34J9B2>
2018-07-05 11:35:11 +0200

    etiter candidat

---
+++ ora/src/java/facades/CandidatFacade.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package facades;
+
+import javabeans.Candidat;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author Nouvel utilisateur
+ */
+@Stateless
+public class CandidatFacade extends AbstractFacade<Candidat> {
+
+    @PersistenceContext(unitName = "oraPU")
+    private EntityManager em;
+
+    @Override
+    protected EntityManager getEntityManager() {
+        return em;
+    }
+
+    public CandidatFacade() {
+        super(Candidat.class);
+    }
+    
+}
